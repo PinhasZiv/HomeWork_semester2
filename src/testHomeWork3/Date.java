c0f64c32c84140862f57e5a8ea13a28634cd03a6
Pinhas <piniziv8@gmail.com>
2020-04-13 10:57:01 +0300

    add another new date class

--- src/testHomeWork3/Date.java
+++ src/testHomeWork3/Date.java
@@ -1,10 +1,12 @@
 package testHomeWork3;
 
 public class Date {
+
 	// declaring variables
 	private int day;
 	private int month;
 	private int year;
+	private final int[] months = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	// c-default
 	public Date() {
@@ -49,12 +51,16 @@ public class Date {
 			System.out.println("Invalid Day Value");
 			this.day = 1;
 		} else
-
 			this.day = day;
 	}
 
 	public void setMonth(int month) {
-		// checking the bounds of set month
+		// checking the bounds of set month and check the last day on this month bound
+		if(months[month-1] < day) {
+			System.out.println("Invalid day in month");
+			this.day = 1;
+		}
+		
 		if (month < 1 || month > 12) {
 			System.out.println("Invalid Month Value");
 			month = 1;
@@ -74,64 +80,71 @@ public class Date {
 	public String toString() {
 
 		// using string format for showing two digits (xx/yy/zzzz)
-
-		return String.format("%02d/%02d/%02d/%s", day, month, year, strMonth(month));
+		printMonth(month);
+		return String.format("%02d/%02d/%02d", day, month, year);
 	}
 
 	// Gets an integer and converts it into a verbal form by switch case format
-	public String strMonth(int month) {
+	public void printMonth(int month) {
 
 		switch (month) {
 
 		case 1:
-			return "January";
-
+			System.out.println("The month is: Yanuary");
+			break;
 		case 2:
-			return "February";
-
+			System.out.println("The month is: February");
+			break;
 		case 3:
-			return "March";
-
+			System.out.println("The month is: March");
+			break;
 		case 4:
-			return "April";
-
+			System.out.println("The month is: April");
+			break;
 		case 5:
-			return "May";
+			System.out.println("The month is: May");
+			break;
 
 		case 6:
-			return "June";
+			System.out.println("The month is: June");
+			break;
 
 		case 7:
-			return "July";
+			System.out.println("The month is: July");
+			break;
 
 		case 8:
-			return "August";
+			System.out.println("The month is: August");
+			break;
 
 		case 9:
-			return "September";
+			System.out.println("The month is: Semptember");
+			break;
 
 		case 10:
-			return "October";
+			System.out.println("The month is: October");
+			break;
 
 		case 11:
-			return "Novemver";
+			System.out.println("The month is: November");
+			break;
 
 		case 12:
-			return "December";
+			System.out.println("The month is: December");
+			break;
 
 		}
-		return "";
+
 	}
 
 	public boolean isLeapYear(int year) {
-		
-		
+
 		int leapCheck = 0;
-	
+
 		leapCheck = year % 19;
-		
+
 		boolean isLeap = false;
-		
+
 		if (leapCheck == 3 || leapCheck == 6 || leapCheck == 8 || leapCheck == 11 || leapCheck == 14 || leapCheck == 17
 				|| leapCheck == 0) {
 			isLeap = true;
